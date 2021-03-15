ed36b3c293aec9ad3ff4c4400acef2d33526b1c8
clement <clementpain@outlook.com>
2021-03-15 15:36:15 +0100

    add tests
    Signed-off-by: clement <clementpain@outlook.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_110315) do
+ActiveRecord::Schema.define(version: 2021_03_15_124907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2021_03_15_110315) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "role", default: 0
   end
 
   add_foreign_key "profiles", "users"
--- spec/models/town_hall_profile_spec.rb
+++ spec/models/town_hall_profile_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe TownHallProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create profile" do
+    user = User.new(email: 'test@test.fr', password: 'testfr', password_confirmation: 'testfr', role: 1)
+    user.town_hall_profile = TownHallProfile.new(name: 'Mairie de test')
+    user.save
+
+    expect(User.count).to eq(1)
+    expect(user.town_hall_profile.name).to eq('Mairie de test')
+    expect(TownHallProfile.count).to eq(1)
+  end
+
+  it "should not create profile: no user" do
+    TownHallProfile.create(name: 'Mairie de test')
+
+    expect(TownHallProfile.count).to eq(0)
+  end
+
+  context 'check validations' do
+    it 'should not create profile: user already taken' do
+      user = User.create(email: 'test@test.fr', password: 'testfr', password_confirmation: 'testfr', role: 1)
+      profile1 =  TownHallProfile.create(name: 'Mairie de test', user_id: user.id)
+      profile2 =  TownHallProfile.create(name: 'Mairie de test', user_id: user.id)
+
+      expect(TownHallProfile.count).to eq(1)
+    end
+
+    it 'should not create profile: user is a town_hall' do
+      user = User.create(email: 'test@test.fr', password: 'testfr', password_confirmation: 'testfr', role: 0)
+      profile =  TownHallProfile.create(name: 'Mairie de test', user_id: user.id)
+
+      expect(TownHallProfile.count).to eq(0)
+    end
+  end
 end
