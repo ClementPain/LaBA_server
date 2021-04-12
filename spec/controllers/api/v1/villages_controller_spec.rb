75ad705ebdc153b9ce38ac97dd96b56bf547e3fb
clement <clementpain@outlook.com>
2021-04-12 17:56:01 +0200

    change name test village
    Signed-off-by: clement <clementpain@outlook.com>

--- spec/controllers/api/v1/villages_controller_spec.rb
+++ spec/controllers/api/v1/villages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::VillagesController, type: :controller do
-  describe '#create' do
+  describe '#index' do
 
     context '#valid call' do
       before do
